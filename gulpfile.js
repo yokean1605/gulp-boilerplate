c539529f89108d00222771d56ba24663fcba3d3c
P1x3l5M4773r <pixelsmatter1@gmail.com>
2018-01-29 20:49:18 +0000

    Defined basic SASS compilation task

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,16 @@ Dependencies
 
 const gulp = require('gulp'),
 sass = require('gulp-sass');
+
+
+/**
+
+Compiles SASS
+
+**/
+
+gulp.task('sass', () => {
+	return gulp.src('./src/sass/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./dist/css'));
+});
